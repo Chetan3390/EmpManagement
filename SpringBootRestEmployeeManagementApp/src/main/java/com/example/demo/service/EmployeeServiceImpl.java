e895a81551ec1fdc5763c4e1623596a3d0134032
Chetan K <chetansai3390@gmail.com>
2025-01-23 12:57:37 +0530

    2nd commit

--- SpringBootRestEmployeeManagementApp/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
+++ SpringBootRestEmployeeManagementApp/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -1,5 +1,26 @@
 package com.example.demo.service;
 
-public class EmployeeServiceImpl implements EmployeeService{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repo.EmployeeRepo;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	EmployeeRepo repo;
+
+	@Override
+	public String addEmployee(Employee employee) {
+		Employee emp = repo.save(employee);
+		if (emp != null) {
+			return "Employee Saved Successfully";
+		}else {
+			return "Failed to Save Employee";
+		}
+
+	}
 
 }
