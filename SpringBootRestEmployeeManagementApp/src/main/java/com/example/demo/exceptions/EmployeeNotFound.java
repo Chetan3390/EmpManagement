f5ccf20eb4bb876024de309df1fab84401a5b246
Chetan K <chetansai3390@gmail.com>
2025-01-23 11:00:51 +0530

    First Commit

---
+++ SpringBootRestEmployeeManagementApp/src/main/java/com/example/demo/exceptions/EmployeeNotFound.java
@@ -0,0 +1,10 @@
+package com.example.demo.exceptions;
+
+public class EmployeeNotFound extends Exception {
+	public EmployeeNotFound(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+	
+
+}
